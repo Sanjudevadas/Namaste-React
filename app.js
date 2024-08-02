fd86d85ae22f18e7a4f62700b54cc84116c05d00
Sanju Devadas <sanjudevadas11@gmail.com>
2024-08-02 12:28:48 +0530

    episode 3 code of namste react

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const Parent = React.createElement(
-  "div",
-  {},
-  (child = React.createElement(
-    "div",
-    {},
-    (heading = React.createElement("h1", {}, "hello from h1"))
-  ))
-);
+const heading= React.createElement("h1",{id:"heading"},"Namste React❄️");
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(Parent);
+// JSX code is transplied before it reaches js engine by parcel -Babel
+
+//JSX =>Babel Transpiled it to React.createElement =>ReactElement-JS Object => HTMLElement(render)
+
+const JsxHeading=()=><h1 id='heading'>Namste React From JSX</h1>
+const number=true;
+const HeadingComponent = ()=>{
+  return(
+    <div>
+    <JsxHeading/>
+    <h2>{number?"HY true" : "hey false"}</h2>
+    <h1>namste From functional component</h1>
+    </div>  )
+ 
+}
+
+
+const root= ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent/>)
+
+
